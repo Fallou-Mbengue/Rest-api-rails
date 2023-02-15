d9cd967aa8a4b684bc412d4ff3349ec6ee9ea5e4
fallou-mbengue <falloumbengue96@gmail.com>
2023-02-15 10:17:47 +0000

    paginnation

--- app/controllers/ArticlesController.rb
+++ app/controllers/ArticlesController.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
+    include Paginable
     def index
-        articles = Article.all
-        render json: serializer.new(articles), status: :ok
+        article = Article.recent
+        render json: serializer.new(article), status: :ok
+        paginated = paginate(Article.recent)
+        render_collection(paginated)
+    end
+
+    def show
+        article = Article.find(params[:id])
+        render json: serializer.new(article)
     end
 
     def serializer
